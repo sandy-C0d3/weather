413fc809ce5596ae481f090748776351e261c633
sandy-C0d3 <97771022+sandy-C0d3@users.noreply.github.com>
2022-02-04 23:49:59 -0500

    adding gifs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,9 +11,11 @@ import { Subscription } from 'rxjs';
 })
 export class AppComponent {
   title = 'weather';
+  images_location = "../assets/images/"
   subscribe : Subscription | undefined;
   data : any = {};
   enableDiv = false;
+  imagesource : any;
   constructor(private s : TempService) {}
 
   search(){
@@ -21,6 +23,20 @@ export class AppComponent {
       console.log(response)
       this.data=response.body
       this.enableDiv=true
+      if(this.data.current.weather_descriptions [0] == "Sunny" || "Clear" && (this.data.current.is_day == "yes")){
+        this.imagesource= this.images_location+"sun.gif"
+      }
+      else{
+        this.imagesource=this.images_location+"moon.gif"
+      }
+      
+      if(this.data.current.weather_descriptions [0] == "Light Rain Shower" || "Light drizzle")
+      {
+        this.imagesource= this.images_location+"raining.gif"
+      }
+      if(this.data.current.weather_descriptions[0] == "Light Snow"){
+        this.imagesource= this.images_location+"snow.gif"
+      }
 
     },(error:any)=>{
       console.log(error)
